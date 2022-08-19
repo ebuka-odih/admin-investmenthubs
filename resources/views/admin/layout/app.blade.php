f1d7ebc55ba59c72a83add9dbf79b351e47ef0a3
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-08-19 06:40:41 +0100

    commit changes

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Dashboard | Uni Trust Invest</title>
+    <title>Dashboard | Trust investment hub</title>
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
     <!-- Bootstrap 4.0-->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
