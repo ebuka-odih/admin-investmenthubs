57b96722f8da6ff6bf5bfd9221a97e7d1cedc28a
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-08-19 06:56:56 +0100

    commit changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Trust Investment Cap | Login</title>
+    <title>Trust Investment Hub | Login</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
